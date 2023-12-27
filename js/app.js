d6efbb2958e73d45cd932144cde9ab117960a964
SanamKhudoyberdieva <khudoyberdievasanam@gmail.com>
2023-12-27 21:18:40 +0500

    product page

--- js/app.js
+++ js/app.js
@@ -151,6 +151,78 @@ $(".pr-feedback-slider").slick({
   ]
 });
 
+$('.pr-pv-lg-slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  prevArrow:
+    '<button class="btn carousel-control-prev-icon pr-slick-prev" aria-hidden="true" role="button" data-slide="prev"><svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7 13L1 7L7 1" stroke="#454168" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></button>',
+  nextArrow:
+    '<button class="btn carousel-control-next-icon pr-slick-next" aria-hidden="true" role="button" data-slide="next"><svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3. org/2000/svg"><path d="M1 13L7 7L1 1" stroke="#454168" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></button>',
+  fade: true,
+  asNavFor: '.pr-pv-sm-slider'
+});
+
+$('.pr-pv-sm-slider').slick({
+  slidesToShow: 4.5,
+  slidesToScroll: 1,
+  asNavFor: '.pr-pv-lg-slider',
+  dots: false,
+  centerMode: true,
+  focusOnSelect: true,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3.5,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 3.5,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
+$(".pr-pv-analogies-slider").slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  prevArrow: false,
+  nextArrow: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2.1,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2.1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
 let t = $("#back-top");
 $(window).on("scroll", function () {
   ($scrollTop = $(window).scrollTop()),
