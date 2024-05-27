14a5abf8a75a349c3349aedd9471a4be151aeb62
SanamKhudoyberdieva <khudoyberdievasanam@gmail.com>
2024-05-27 15:09:48 +0500

    menu modal

--- js/app.js
+++ js/app.js
@@ -5,9 +5,9 @@ $(".pr-products-slick").slick({
     slidesToShow: 6,
     slidesToScroll: 1,
     prevArrow:
-      '<button class="btn carousel-control-prev-icon pr-slick-prev" aria-hidden="true" role="button" data-slide="prev"><svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7 13L1 7L7 1" stroke="#454168" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></button>',
+      '<button class="slick-arrow slick-prev slick-disabled slick-prev slick-arrow lc-black" aria-hidden="true" role="button" data-slide="prev"><svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7 13L1 7L7 1" stroke="#454168" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></button>',
     nextArrow:
-      '<button class="btn carousel-control-next-icon pr-slick-next" aria-hidden="true" role="button" data-slide="next"><svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3. org/2000/svg"><path d="M1 13L7 7L1 1" stroke="#454168" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></button>',
+      '<button class="slick-arrow slick-next slick-next slick-arrow lc-black" aria-hidden="true" role="button" data-slide="next"><svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3. org/2000/svg"><path d="M1 13L7 7L1 1" stroke="#454168" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></button>',
     responsive: [
       {
         breakpoint: 1024,
